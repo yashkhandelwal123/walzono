715053ee87547757048f4ba7207c5ed108166434
ShubhamAgarwal-11 <shubham2002jindal@gmail.com>
2025-01-17 15:49:51 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const experss = require("express");
+const app = experss();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
