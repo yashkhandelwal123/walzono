7c24dc62cdf9c13cb3398dea227cebc1ba5c157b
ShubhamAgarwal-11 <shubham2002jindal@gmail.com>
2025-03-17 12:46:16 +0530

    changes in constent.js

--- frontend/src/utils/constent.js
+++ frontend/src/utils/constent.js
@@ -1,3 +1,3 @@
-export const USER_API_END_POINT = "http://localhost:4000/api/user"
+export const USER_API_END_POINT = "https://localhost:4000/api/user"
 
 export const PARTNER_API_END_POINT = "http://localhost:4000/api/partner"
